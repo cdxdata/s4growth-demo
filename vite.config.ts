1b80d0342e1245e21589b746bf9a031f68d14041
Tobey <lead_engineer@cdxdata.com>
2026-09-24 11:25:20 +0100

    edited base field

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/', // Use '/' if deploying to a user page (username.github.io), or '/repo-name/' if it is a project repository
+  base: '/s4growth-demo/',
   plugins: [react()],
   resolve: {
     alias: {
